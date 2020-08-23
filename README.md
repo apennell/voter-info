b7069376b14eb8b03a6ea9052a247782516e439b
Annie Pennell <annie@coverhound.com>
2020-08-23 14:01:41 -0700

    Get active section
    Monitor scrolling to determine which section is active and update
    section and nav classes to show

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
 
 [Project Rubric](https://review.udacity.com/#!/rubrics/2658/view)
+
+Scroll behavior is achieved through use of native HTML + CSS functionality to preserve accessibility while improving UX.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-/**
- * Exercise programmatically builds navigation, scrolls to anchors from navigation, and highlights
- * section in viewport upon scrolling.
- */
+// Exercise programmatically builds navigation and highlights section in viewport upon scrolling.
 
 // Create the navigation menu
 const navList = document.querySelector('#navbar-list');
@@ -10,16 +7,40 @@ const sections = document.querySelectorAll('.section-wrapper');
 const navItems = document.createDocumentFragment();
 sections.forEach(({id, dataset: {nav}}) => {
   const li = document.createElement('li');
+  li.id = `link-${id}`;
   const link = `<a href="#${id}">${nav}</a>`;
   li.innerHTML = link;
   navItems.appendChild(li);
 });
 navList.appendChild(navItems);
 
-// Add class 'active' to section when near top of viewport
+// Toggle which section has class 'active-section' when new class nears top of viewport on scroll
+window.addEventListener('scroll', () => {
+  // Reverse section order to find the lowest section near the top of window first
+  const reverseSections = [...sections].reverse();
 
-// Scroll to anchor ID using scrollTO event
+  for (let i = 0; i < reverseSections.length; i++) {
+    const section = reverseSections[i];
+    const sectionPosition = section.getBoundingClientRect();
+    const inView = sectionPosition.top < 250;
+    if (inView) {
+      // Only update active classes if changing which section is active
+      if (!section.classList.contains('active-section')) {
+        const lastActiveSection = document.querySelector('.active-section');
+        lastActiveSection &&
+          lastActiveSection.classList.remove('active-section');
+        const lastActiveNav = document.querySelector('.active-nav');
+        lastActiveNav && lastActiveNav.classList.remove('active-nav');
 
-// Scroll to section on link click
+        section.classList.add('active-section');
+        const activeNavItem = document.querySelector(`#link-${section.id}`);
+        activeNavItem.classList.add('active-nav');
+      }
 
-// Set sections as active
+      // Stop searching once active view is found
+      break;
+    }
+  }
+});
+
+// Scroll to top
