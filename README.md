44ceb1104800a3eb40b9555983a5f74c1a7ad9b0
Annie Pennell <annie@coverhound.com>
2020-08-23 18:08:38 -0700

    Refactor and add scroll top button

--- README.md
+++ README.md
@@ -2,8 +2,28 @@
 
 ## Project 2 for Udacity Frontend Nanodegree course
 
-This project requires you to build a multi-section landing page, with a dynamically updating navigational menu based on the amount of content that is added to the page.
+This project is a demonstration of accessing and manipulating the DOM using JavaScript.
+
+Core requirements:
+
+- Navigation built dynamically using an unordered list, based on document `section`s
+- At least 4 sections on page, with currently active section styled
+- Clicking an item from the navigation scrolls to the appropriate section
+
+Additional features:
+
+- Style active navigation item
+- "Scroll to top" button
+- Custom design and content
 
 [Project Rubric](https://review.udacity.com/#!/rubrics/2658/view)
 
-Would be better to implement scroll behavior through use of native HTML + CSS functionality to preserve accessibility while improving UX, but achieved through JS instead to satisfy project requirements.
+The best way to implement the scroll-to-section behavior would be to use the native behavior of HTML and CSS, which preserves accessibility while improving UX ([reference](https://css-tricks.com/snippets/jquery/smooth-scrolling/)). All that would be required is this CSS addition:
+
+```css
+html {
+  scroll-behavior: smooth;
+}
+```
+
+However, it was clear that the project requirements wanted this done using JavaScript selectors and scrolling, so I implemented it that way rather than the best way.
--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@
  * and highlights section in viewport upon scrolling.
  */
 
-// Create the navigation menu
+// === Create the navigation menu ===
 const navList = document.querySelector('#navbar-list');
 const sections = document.querySelectorAll('.section-wrapper');
 
 const navItems = document.createDocumentFragment();
+// Create a `li` with anchor for each section found in doc
 sections.forEach(({id, dataset: {nav}}) => {
   const li = document.createElement('li');
   li.id = `link-${id}`;
@@ -15,11 +16,19 @@ sections.forEach(({id, dataset: {nav}}) => {
   li.innerHTML = link;
   navItems.appendChild(li);
 });
+
+// Don't append menu items until navList is complete to avoid many repaints
 navList.appendChild(navItems);
 
-// Scroll to section on nav click
+// === Scroll to selected section on nav click ===
 const scrollToSection = (e) => {
   e.preventDefault();
+
+  /**
+   * Note: this would be much better to handle with CSS, but wouldn't satisfy project requirements;
+   * this will lose focus in an unexpected way, but is a common implementation despite loss
+   * of full accessibility
+   */
   document.querySelector(e.target.getAttribute('href')).scrollIntoView({
     behavior: 'smooth',
   });
@@ -29,7 +38,20 @@ document.querySelectorAll('.nav-link').forEach((link) => {
   link.addEventListener('click', scrollToSection);
 });
 
-// Toggle which section has class 'active-section' when new class nears top of viewport
+// === Scroll to top on click ===
+const scrollToTop = () => {
+  document.querySelector('.grid-wrapper').scrollIntoView({
+    behavior: 'smooth',
+  });
+};
+
+const topButton = document.querySelector('#top-button');
+topButton.addEventListener('click', scrollToTop);
+
+/**
+ * Toggle active:
+ * set which section has class 'active-section' when new section nears top of viewport
+ */
 const setActiveSection = () => {
   // Reverse section order to find the lowest section near the top of window first
   const reverseSections = [...sections].reverse();
@@ -38,7 +60,7 @@ const setActiveSection = () => {
     const section = reverseSections[i];
     const inView = section.getBoundingClientRect().top < 250;
     if (inView) {
-      // Only update active classes if changing which section is active
+      // Only update active classes if changing which section is active to avoid unnecessary repaints
       if (!section.classList.contains('active-section')) {
         const lastActiveSection = document.querySelector('.active-section');
         lastActiveSection &&
@@ -58,5 +80,3 @@ const setActiveSection = () => {
 };
 
 window.addEventListener('scroll', setActiveSection);
-
-// Scroll to top
